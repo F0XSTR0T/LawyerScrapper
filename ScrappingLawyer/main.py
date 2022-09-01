73787cfac25472e22384cc9514c3172d036acddc
Julien2L <julien.lehoussine-labatut@epitech.eu>
2022-09-01 14:29:20 +0200

    initial commit

---
+++ ScrappingLawyer/main.py
@@ -0,0 +1,56 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_all_pages():
+    urls = []
+    page_number = 1
+
+    for i in range(104):
+        i = f"https://www.barreaudenice.com/annuaire/avocats/?fwp_paged={page_number}"
+        page_number += 1
+        urls.append(i)
+
+    return urls
+
+
+def parse_lawyer(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    avocats = soup.find_all('div', class_='callout secondary annuaire-single')
+
+    for avocat in avocats:
+        try:
+            nom = avocat.find('h3').text.strip()
+        except AttributeError as e:
+            nom = " "
+        adresse = avocat.find('span', class_='adresse').text.strip()
+        try:
+            adresse_finale = re.sub(r"\s", "", adresse)
+        except AttributeError as e:
+            adresse = " "
+        try:
+            telephone = avocat.find('span', class_='telephone').text.strip()
+        except AttributeError as e:
+            telephone = " "
+        try:
+            email = avocat.find('span', class_='email').a.text.strip()
+        except AttributeError as e:
+            email = " "
+        chemin = r"/Users/julien/Documents/project/ScrappingLawyer/annuaire_avocat.txt"
+        with open(chemin, "a") as f:
+            f.write(f"{nom}\n")
+            f.write(f"{adresse_finale}\n")
+            f.write(f"{telephone}\n")
+            f.write(f"{email}\n\n")
+
+
+def parse_all_lawyer():
+    pages = get_all_pages()
+    for page in pages:
+        parse_lawyer(url=page)
+        print(f"On scrape la page {page}")
+
+parse_all_lawyer()
